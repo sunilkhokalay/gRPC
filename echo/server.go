250249965334910ac65fdcd2f00229eda5394565
sunilkhokalay <sunilkhokalay@gmail.com>
2018-01-15 09:06:22 +0530

    Added echo Server.go

---
+++ echo/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "gRPC/echo/echo"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+
+type server struct{}
+
+
+func (s *server) EchoBack(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{MsgReponse: in.MsgRequest}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
